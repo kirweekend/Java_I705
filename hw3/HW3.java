4c3ad34b714fc77460bc5032a55da918ddbc6771
Kirill <kirweekend@mail.ru>
2017-06-02 15:37:21 +0300

    sorting

--- hw3/HW3.java
+++ hw3/HW3.java
@@ -1,14 +1,18 @@
-package hw3;
+package HW3;
 
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * Created by kirweekend on 4/25/17.
@@ -21,6 +25,9 @@ public class HW3 {
         int c = 0;
         try {
             PrintWriter outputStream = new PrintWriter(filename);
+            outputStream.println("        News       ");
+            outputStream.println("-------------------");
+
             for (Element headline : doc.select("div.headline > a[href]")){
                 c ++;
                 outputStream.println(c + " " + headline.text());
@@ -36,7 +43,20 @@ public class HW3 {
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        String content = new String(Files.readAllBytes(Paths.get("headlines.txt")));
+        Path path = Paths.get("headlines.txt");
+        File news = new File("headlines.txt");
+
+        if (news.exists()){
+            java.util.List<String> rlines = Files.readAllLines(path);
+            rlines.add(0, content);
+            Files.write(path, rlines);
+        } else {
+            Files.write(path, content.getBytes(), StandardOpenOption.CREATE);
+            //CREATE = create new file if not exist; APPEND = add new data to same file when program is run
+        }
     }
+
 }
 
 
