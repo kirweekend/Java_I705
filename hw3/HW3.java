13362bef85b0ac8378bb01d7bab9eb5a51a7ba71
Kirill <kirweekend@mail.ru>
2017-06-02 18:15:07 +0300

    fix

--- hw3/HW3.java
+++ hw3/HW3.java
@@ -1,65 +1,51 @@
 package HW3;
 
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.util.Date;
 
-/**
- * Created by kirweekend on 4/25/17.
- */
 public class HW3 {
-    public static void main(String[] args) throws IOException {
-        String filename = "headlines.txt";
-        Document doc;
-        doc = Jsoup.connect("http://www.washingtonpost.com").get();
-        int c = 0;
+
+    static Document doc;
+
+    static {
         try {
-            PrintWriter outputStream = new PrintWriter(filename);
-            outputStream.println("        News       ");
-            outputStream.println("-------------------");
-
-            for (Element headline : doc.select("div.headline > a[href]")){
-                c ++;
-                outputStream.println(c + " " + headline.text());
-                outputStream.println(" > " + headline.attr("href"));
-                outputStream.println();
-                if (c == 10) {
-                    break;
-                }
-            }
-
-            outputStream.close();
-        }
-        catch (FileNotFoundException e) {
+            doc = Jsoup.connect("http://www.washingtonpost.com").get();
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        String content = new String(Files.readAllBytes(Paths.get("headlines.txt")));
-        Path path = Paths.get("headlines.txt");
-        File news = new File("headlines.txt");
-
-        if (news.exists()){
-            java.util.List<String> rlines = Files.readAllLines(path);
-            rlines.add(0, content);
-            Files.write(path, rlines);
-        } else {
-            Files.write(path, content.getBytes(), StandardOpenOption.CREATE);
-            //CREATE = create new file if not exist; APPEND = add new data to same file when program is run
         }
     }
 
+    public static void main(String[] args) throws IOException {
+        String filename = "headlines.txt";
+        int c = 0;
+        PrintWriter outputStream = new PrintWriter(new FileOutputStream(new File(filename),
+                true /*check if file exist and append*/));
+
+        String content = new String(Files.readAllBytes(Paths.get("headlines.txt")));
+        System.out.println(content);
+
+        outputStream.println("        News       " + new Date());
+        outputStream.println("-----------------------------------");
+
+        for (Element headline : doc.select("div.headline > a[href]")) {
+            c++;
+            outputStream.println(c + " " + headline.text());
+            outputStream.println(" > " + headline.attr("href"));
+            outputStream.println();
+            if (c == 10) {
+                break;
+            }
+        }
+        outputStream.close();
+
+    }
 }
-
-
-
-
-
